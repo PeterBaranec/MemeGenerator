2c71539a4c085664c9ae8b4f363dda7263cbe09e
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-04-09 13:07:30 +0200

    generate meme image

--- meme-generator/src/components/Form.jsx
+++ meme-generator/src/components/Form.jsx
@@ -1,13 +1,48 @@
+import { useState } from "react";
 import "../components/Form.css";
+import memeData from "../memesData.js";
 
 function Form() {
+  const [memeImage, setMemeImage] = useState("");
+
+  function getMemeImage(e) {
+    e.preventDefault();
+    const memeArray = memeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeArray.length);
+    const url = memeArray[randomNumber].url;
+    setMemeImage(url);
+  }
+
   return (
     <main>
       <form className="form">
-        <input type="text" placeholder="Top text" className="form__input" />
-        <input type="text" placeholder="Bottom text" className="form__input" />
-        <button className="form__btn">Get a new meme image 🖼</button>
+        <div>
+          <label htmlFor="top-text" className="form__label">
+            Top Text
+          </label>
+          <input
+            id="top-text"
+            type="text"
+            placeholder="Shut up"
+            className="form__input"
+          />
+        </div>
+        <div>
+          <label htmlFor="bottom-text" className="form__label">
+            Bottom Text
+          </label>
+          <input
+            id="bottom-text"
+            type="text"
+            placeholder="and take my money"
+            className="form__input"
+          />
+        </div>
+        <button onClick={getMemeImage} className="form__btn">
+          Get a new meme image 🖼
+        </button>
       </form>
+      <img src={memeImage} alt="random meme image" className="meme__image" />
     </main>
   );
 }
